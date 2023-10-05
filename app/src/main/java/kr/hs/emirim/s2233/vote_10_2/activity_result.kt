ef7d9f1dbabb9fd7e11e1da60b041c8c02245007
ulfunhy <myfamily11141@gmail.com>
2023-10-05 20:12:05 +0900

    main result layut 작성

---
+++ app/src/main/java/kr/hs/emirim/s2233/vote_10_2/activity_result.kt
@@ -0,0 +1,11 @@
+package kr.hs.emirim.s2233.vote_10_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_result : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
